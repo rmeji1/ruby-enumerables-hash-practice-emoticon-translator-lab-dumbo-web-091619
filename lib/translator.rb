6a35ef0347f737ca8c047799e1741444c96ced1f
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 12:51:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-  result = { get_meaning : [], 
+  result = { get_meaning : [], get_emoticon: [] }
   emoticons.each_pair do 
   p emoticons
 end
