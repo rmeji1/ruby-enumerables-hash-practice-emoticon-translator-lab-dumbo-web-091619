aa62ea6c92d3388e05975465e3bcf58146432355
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 13:10:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(file_path)
   japanese_index = 1
   
   emoticons.reduce( memo ) do |memo, (key, value)|
-    japanese_emoticon = value[english_index]
-    english_emoticon = value[japanese_index]
+    japanese_emoticon = value[japanese_index]
+    english_emoticon = value[english_index]
     memo[:get_meaning][japanese_emoticon] = key
     memo[:get_emoticon][english_emoticon] = japanese_emoticon
     memo
