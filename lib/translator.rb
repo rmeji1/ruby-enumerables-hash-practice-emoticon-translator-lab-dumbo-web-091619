679a0e766bc5f48e0bedd71715afd0810fad4d30
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 13:08:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   memo = { get_meaning: {}, get_emoticon: {} }
   
-  ENGLISH_INDEX = 0
-  JAPANESE_INDEX = 1
+  english_index = 0
+  japanese_index = 1
   
   memo = emoticons.reduce( memo ) do |memo, (key, value)|
     memo[:get_meaning][value[JAPANESE_INDEX]] = key
