b0021f2c7ed1be3f9b0ba6e507809e888b2904e9
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-28 13:14:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   japanese_emoticon = emoticon_hash[:get_emoticon][emoticon]
-   if  
+  return japanese_emoticon ? japanese_emoticon : ""  
 end
 
 def get_english_meaning
